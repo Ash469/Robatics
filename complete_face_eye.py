fb4340207c6d04f52c1bafb22df6df2b8a58dff3
Ash469 <117721052+Ash469@users.noreply.github.com>
2024-02-07 18:38:23 +0530

    complete_face_eye.py

---
+++ complete_face_eye.py
@@ -0,0 +1,29 @@
+import cv2
+face_cascade=cv2.CascadeClassifier("C:/Users/ASUS/Documents/body_detection_opencv/haarcascade_frontalface_default.xml")#path of casadeclassifier
+
+eye_cascade=cv2.CascadeClassifier("C:/Users/ASUS/Documents/body_detection_opencv/haarcascade_eye_tree_eyeglasses.xml")#path of casadeclassifier
+
+"""img = cv2.imread("C:/Users/ASUS/Desktop/WIN_20231225_19_46_56_Pro.jpg")
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray,1.3,5)
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),3)q
+cv2.imshow('image',img)
+cv2.waitKey(0)"""
+
+capture=cv2.VideoCapture(0)
+while True:
+    success,img=capture.read()
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray,1.3,5)
+    eyes=eye_cascade.detectMultiScale(gray,1.3,5)
+    for (x,y,w,h) in faces:
+       cv2.circle(img,(x+w//2,y+h//2),100,(0,255,0),3)
+    for (x,y,w,h) in eyes:
+       cv2.circle(img,(x+w//2,y+h//2),20,(255,255,0),2)
+
+    cv2.imshow('image',img)
+    if cv2.waitKey(20)==ord('q'):
+        break
+capture.release()
+cv2.destroyAllWindows()
